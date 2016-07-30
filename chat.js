8b4b1a08a4468e3c8b30160fe947f6d811f295d5
Scott Anderson <scott.p.anderson@gmail.com>
2016-07-30 14:40:19 -0700

    initial support for chat menus & layout fixes for chat dropdowns

--- chat.js
+++ chat.js
@@ -1,7 +1,11 @@
 console.log("## chat: entering");
 
-var tabContent;
-var chats;
+var roomList;
+var roomSelect;
+var chatRoom;
+var chatContent;
+
+var watchedRooms = {};
 
 function initDropdownToggles() {
     var dropdownToggles = $("#bbchat-header .dropdown-toggle");
@@ -19,20 +23,134 @@ function initDropdownToggles() {
 	}).remove();	
 }
 
-function initChatContent() {
-	tabContent = $("#bbchat-tab-content");
-	chats = tabContent.find(".chat");
-	layoutChatContent();
-	tabContent.bind("DOMNodeInserted", function() {
-		chats = tabContent.find(".chat");
-		layoutChatContent();
+function createRoom(roomElem) {
+	return {
+		id: roomElem.attr("data-room-id"),
+		title: roomElem.children()[0].title,
+		active: roomElem.hasClass("active")
+	}
+}
+
+function createRoomsSelect() {
+	var wrapper = $('<div/>');
+	var container = $('<label/>');
+	roomSelect = $('<select/>');
+	roomSelect.change(function(ev) {
+		var roomEntry = watchedRooms[ev.target.value];
+		roomEntry.elem.children()[0].click();
+	});
+	container.append(roomSelect);
+	wrapper.append(container);
+	return wrapper;
+}
+
+function createRoomOption(room) {
+	var selected = room.selected ? ' selected="selected"' : '';
+	return $('<option value="' + room.id + '"' + selected + '>' + room.title + '</option>');
+}
+
+function activateRoom(room, active) {
+	room.active = active;
+	var roomEntry = watchedRooms[room.id];
+	if (active) {
+		roomEntry.option.attr("selected", "selected");
+	}
+	else {
+		roomEntry.option.removeAttr("selected");
+	}
+}
+
+function watchRooms() {
+
+	function startWatching(roomEl) {
+		var roomElem = $(roomEl);
+		var room = createRoom(roomElem);
+		var roomOption = createRoomOption(room);
+		roomSelect.append(roomOption);
+		roomElem.bind("DOMSubtreeModified", function(event) {
+			if (room.active !== roomElem.hasClass("active")) {
+				activateRoom(room, !room.active);
+			}
+		});
+		var roomMessages = chatContent.find("div#" + room.id + " .chat");
+		roomMessages.height(chatContent.height() - 43);
+		watchedRooms[room.id] = {
+			room: room,
+			elem: roomElem,
+			option: roomOption
+		};
+	}
+
+	function stopWatching(roomElem) {
+		var roomEntry = watchedRooms[roomElem.attr("data-room-id")];
+		roomEntry.elem.off("DOMSubtreeModified");
+		roomEntry.option.remove();
+		delete watchedRooms[room.id];
+	}
+
+	roomList.children().each(function(index, roomEl) {
+		startWatching($(roomEl));
+	});
+	roomList.bind("DOMNodeInserted", function(event) {
+		startWatching($(event.target));
+	});
+	roomList.bind("DOMNodeRemoved", function(event) {
+		stopWatching($(event.target));
 	});
 }
 
+function leaveRoom(select) {
+	var closeButton = chatContent.find("div.tab-pane.active > div.tab-pane-menu > a.close");
+	closeButton[0].click();
+	select.find('option[value="leave"]').removeAttr("selected");
+	select.find('option[value="message"]').attr("selected", "selected");
+}
+
+function createActionsSelect() {
+	var wrapper = $('<div/>');
+	var container = $('<label/>');
+	var select = $('<select/>');
+	select.append('<option value="messages">Messages</option>');
+	select.append('<option value="people">People</option>');
+	select.append('<option value="leave">Leave Room</option>');
+	select.change(function(ev) {
+		switch(ev.target.value) {
+			case "messages":
+				break;
+			case "people":
+				break;
+			case "leave":
+				leaveRoom(select);
+				break;
+		};
+	});
+	container.append(select);
+	wrapper.append(container);
+	return wrapper;
+}
+
+function initMenus() {
+	var menus = $('<div id="bbchat-menus"></div>');
+	menus.append(createRoomsSelect());
+	menus.append(createActionsSelect());
+	menus.insertBefore(chatContent);
+}
+
 function layoutChatContent() {
-	if (chats && chats.length) {
-		chats.height(tabContent.height() - 43);
+	var messages = chatContent.find(".chat");
+	if (messages && messages.length) {
+		messages.height(chatContent.height() - 43);
 	}
+}
+
+function initChat() {
+	roomList = $("#bbchat-tab-list");
+	chatRoom = $("#bbchat-tabs");
+	chatContent = $("#bbchat-tab-content");
+	initDropdownToggles();
+	initMenus();
+	layoutChatContent();
+	watchRooms();
 }
 
 $(document).ready(function() {
@@ -45,8 +163,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 window.onload = function() {
 	console.log("## chat: window loaded");
-	initDropdownToggles();
-	initChatContent();
+	initChat();
 };
 
 $(window).resize(function() {
